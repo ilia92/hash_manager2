341d407855100004295dc700a4ecf118c35ecd1e
ilia_92 <ilia_92@mail.bg>
2023-09-20 02:32:09 +0300

    working state

--- main.sh
+++ main.sh
@@ -2,19 +2,43 @@
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-source $DIR/functions/workers_to_array.func
-source $DIR/functions/get_real_stats.func
-source $DIR/functions/get_all_stats.func
-source $DIR/functions/print_all_stats.func
+source $DIR/manager.conf
 
-declare -A rigs=()
-declare -a rigNames=()
+source $DIR/functions/workers_to_array.func
+source $DIR/functions/get_rig_stats.func
+source $DIR/functions/print_stats.func #includes print_full_stats() print_short_stats() print_problem_stats()
+source $DIR/functions/sendtext.func
+source $DIR/functions/notify.func
+source $DIR/functions/check_existence.func
 
 workers_file="$DIR/workers.txt"
 
-workers_to_array $workers_file
+declare -A rigs=()
+# This array is used for all rigs names from workers.txt
+declare -a allRigsNames=()
+# This array is used only for selection of rigs
+declare -a chosenNames=()
 
-get_all_stats
-results_table=`print_all_stats`
+workers_to_array $workers_file > /dev/null
+#chosenNames=("${allRigsNames[@]}")
 
-printf "$results_table\n"
+#printf "RIGNAMES: ${chosenNames[2]}"
+
+case "$1" in
+	("") ;;
+        ("help") result="Help" ;;
+        ("pinger") result=`chosenNames=("${@:2}") ; check_existence ; get_rig_stats > /dev/null ; print_short_stats` ;;
+        ("rigres")  ;;
+        ("rigstop") ;;
+        ("rigstart") ;;
+        ("softres")  ;;
+        ("full") result=`chosenNames=("${allRigsNames[@]}") ; get_rig_stats > /dev/null ; print_full_stats` ;;
+	("recheck") result=`chosenNames=("${allRigsNames[@]}") ; get_rig_stats > /dev/null ; notify ; print_full_stats > $hash_checker_results_file; cat $hash_checker_results_file` ;;
+        ("cache") result=`cat $hash_checker_results_file`;;
+        ("cron") result=`chosenNames=("${allRigsNames[@]}") ; get_rig_stats > /dev/null ; notify ; print_full_stats > $hash_checker_results_file; cat $hash_checker_results_file` ;;
+        ("custom1") result=`$DIR/custom_scripts/custom1.sh` ;;
+	(*) result="Unknown command!" ;;
+esac
+
+#printf "RESULT:\n"
+printf "%s\n" "$result"
--- secretary.sh
+++ secretary.sh
@@ -2,40 +2,36 @@
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-source $DIR/manager.conf
-source $DIR/functions/sendtext.func
-api_key=`cat "$api_key_file"`
+source $DIR/main.sh
+#source $DIR/manager.conf
+#source $DIR/functions/sendtext.func
 
-if [ -z "$STY" ]; then
-printf "Bot started in background\n"
-screen -S secretary_bot -X quit > /dev/null
-exec screen -dm -S secretary_bot /bin/bash $0
-fi
+#if [ -z "$STY" ]; then
+#printf "Bot started in background\n"
+#screen -S secretary_bot -X quit > /dev/null
+#exec screen -dm -S secretary_bot /bin/bash $0
+#fi
 
 refresh_rate=1
 
-curl --silent https://api.telegram.org/bot$api_key/getMe | jq
-username=`curl --silent https://api.telegram.org/bot$api_key/getMe | jq -M -r .result.username`
+curl --silent https://api.telegram.org/bot$telegram_api_key/getMe | jq
+username=`curl --silent https://api.telegram.org/bot$telegram_api_key/getMe | jq -M -r .result.username`
 date
 
 help_section="
 /help - Prints this text
-/reward - Prints average network reward
-/price - Prints ETH current price
 /pinger name - Check if rig is UP
 /rigres name - Restarts rig
 /softres name - Soft restart for a rig
 /full - Check all rigs
 /recheck - Rechecks rig hashrate
-/renull - Clear memory and start notifying again
 /cache - Shows cached results
-/switch - changes the pool
 "
 
 while [ 1 ]
 do
 
-curr_message=`curl --silent -s "https://api.telegram.org/bot$api_key/getUpdates?timeout=600&offset=$update_id"`
+curr_message=`curl --silent -s "https://api.telegram.org/bot$telegram_api_key/getUpdates?timeout=600&offset=$update_id"`
 last_upd_id=`printf "$curr_message" |  jq '.result | .[] | .update_id' | tail -1`
 
 if [[ $update_id -le $last_upd_id ]]; then
@@ -46,36 +42,31 @@ curr_message_text=`printf "$curr_message" | jq -r '.result | .[].message.text' |
 if [[ "$curr_message_text" ]]; then
 printf "Message received: $curr_message_text\n"
 # clear last message
-curl -s "https://api.telegram.org/bot$api_key/getUpdates?offset=$update_id"  >/dev/null 2>&1
+curl -s "https://api.telegram.org/bot$telegram_api_key/getUpdates?offset=$update_id"  >/dev/null 2>&1
 fi
 
 command=`echo $curr_message_text | grep -o '\/.*' | awk {'print $1'} | sed "s|@$username||g"`
-arg=`echo $curr_message_text | awk {'print $2" "$3" "$4'}`
+# Get everything after the 1st
+arg=`echo $curr_message_text | awk '{$1=""}1'`
 
 printf "$command and $arg"
 
 case "$command" in
 	("") ;;
-	("/test") result="test PASS!" ;;
+	("/test") result="Telegram API Test PASS!" ;;
         ("/help") result="$help_section" ;;
-        ("/reward") rew_info=`curl --silent https://whattomine.com/coins/151.json` ; result=`printf "Average ETH block reward:\nNow: $(echo $rew_info | jq .block_reward | grep -o "^.*\...")\n3h:   $(echo $rew_info | jq .block_reward3 | grep -o "^.*\...")\n7h:   $(echo $rew_info | jq .block_reward7 | grep -o "^.*\...")\n24h:  $(echo $rew_info | jq .block_reward24 | grep -o "^.*\...")"` ;;
-        ("/price") result="ETH price now: $(curl --silent https://api.kraken.com/0/public/Ticker -d 'pair=etheur' | jq -r .result.XETHZEUR.c[] | head -1 | grep -o "^.*\...") EUR" ;;
-        ("/pinger") result=`$DIR/pinger.sh $arg` ;;
-        ("/rigres") result=`$DIR/rigres.sh $arg --notify` ;;
-        ("/softres") result=`$DIR/softres.sh $arg --notify` ;;
-        ("/full") result=`$DIR/hash_checker.sh --full | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" | grep -v GENERATED | awk {'printf $1"   "$2"   "$3"   "$4"\n"'}  | sed '/=START=/c\==============='| sed '/=END=/c\==============='` ;;
-	("/recheck") $DIR/hash_checker.sh ; $DIR/telegram_notifier.sh ; result="Recheck done! New result: /cache" ;;
-        ("/renull") rm $DIR/.workers_down ; $DIR/hash_checker.sh ; $DIR/telegram_notifier.sh ;;
-        ("/cache") result=`cat $DIR/cache.txt | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" | awk {'printf $1"   "$2"   "$3"   "$4"\n"'} | sed '/=START=/c\==============='| sed '/=END=/c\==============='` ;;
-        ("/switch") result=`$DIR/pool_switcher.sh $arg` ;;
-        ("/thankyou") result="My pleasure, Master :)" ;;
-	("/wrong") result="I'll do my best next time!" ;;
-        ("/routeadd") result=` ./routeadd.sh` ;;
+        ("/pinger") result=`chosenNames=($arg) ; check_existence ; get_rig_stats > /dev/null ; print_short_stats`;;
+        ("/rigres") result=`` ;;
+        ("/softres") result=`` ;;
+        ("/full") result=`chosenNames=("${allRigsNames[@]}") ; get_rig_stats > /dev/null ; print_short_stats` ;;
+	("/recheck") chosenNames=("${allRigsNames[@]}") ; get_rig_stats > /dev/null ; notify ; print_short_stats > $hash_checker_results_file ; result="Recheck done! New result: /cache" ;;
+        ("/cache") result=`cat $hash_checker_results_file` ;;
+        ("/custom1") result=`$DIR/custom_scripts/custom1.sh` ;;
 	(*) result="Unknown command!" ;;
 esac
 
 if [[ "$result" ]]; then
-#printf "Result:\n$result"
+printf "\nResult:\n$result"
 sendtext "$result"
 fi
 
